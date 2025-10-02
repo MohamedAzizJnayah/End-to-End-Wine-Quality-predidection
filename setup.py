eef1025e9e17e0c6fe88b7a1c63e38518e3064e3
mohamedazizjnayah <mohamedazizjnayah@gmail.com>
2025-10-02 17:25:20 +0100

    requirement dependecy added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import setuptools
+
+setup(
+    name="End_to_End_Wine_Quality_Prediction",
+    version="0.1",
+    author="Mohamed Aziz Jnayah",
+    author_email="mohamedazizjnayah@gmail.com",
+    description="Un projet de machine learning",
+    url="https://github.com/MohamedAzizJnayah/End-to-End-Wine-Quality-predidection",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+    
+)
